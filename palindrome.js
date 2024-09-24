9c43bb1fe0dd0f5896ca46708b891ecdac20a75b
Nguyen Thang <thang.nd0226@gmail.com>
2024-09-24 21:17:59 +0700

    change algorithm to using 2 pointers instead of recursive

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,26 @@
+// function isPalindrome(s) {
+//   let n = s.length;
+
+//   if (n <= 1) return true;
+
+//   if (s[0] === s[n - 1]) {
+//     let s_new = s.slice(1, n - 1);
+//     return isPalindrome(s_new);
+//   } else return false;
+// }
+
 function isPalindrome(s) {
   let n = s.length;
+  let start_ind = 0;
+  let end_ind = n - 1;
 
-  if (n <= 1) return true;
+  while (start_ind < end_ind) {
+    if (s[start_ind] !== s[end_ind]) return false;
+    start_ind++;
+    end_ind--;
+  }
 
-  if (s[0] === s[n - 1]) {
-    let s_new = s.slice(1, n - 1);
-    return isPalindrome(s_new);
-  } else return false;
+  return true;
 }
 
 let word = prompt("Enter a word:");
